b1fb9c1f7de12490acbab03292fb72011ac41f43
Karan Samani <samanik@vmware.com>
2024-06-25 10:51:14 +0100

    better error handling for missing directories

--- pkg/rclone/core.go
+++ pkg/rclone/core.go
@@ -1,9 +1,9 @@
 package rclone
 
 import (
-	"errors"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/ansel1/merry/v2"
 	"github.com/go-logr/logr"
@@ -52,8 +52,8 @@ func (rclone *RcloneInstance) RunVersion() error {
 
 // RunSync runs the rclone sync command with opinionated default arguments
 func (rclone *RcloneInstance) RunSync(logFileSyncPath, logFileSyncCombinedReportPath string,
-	sourceDir, destDir string, extraSyncArgs []string) error {
-
+	sourceDir, destDir string, extraSyncArgs []string,
+) error {
 	cmdArgs := []string{
 		"sync",
 		sourceDir, destDir,
@@ -71,6 +71,9 @@ func (rclone *RcloneInstance) RunSync(logFileSyncPath, logFileSyncCombinedReport
 	rclone.Log.Info("running command", "cmd", cmd.String())
 	output, err := cmd.CombinedOutput()
 	if err != nil {
+		if strings.Contains(err.Error(), "exit status 3") {
+			return merry.Errorf("directory does not exist: %w", err)
+		}
 		return merry.Errorf("failed to run command with output [%s]: %w", string(output), err)
 	}
 	rclone.Log.Info("finished running command", "cmd", cmd.String())
@@ -85,17 +88,16 @@ func (rclone *RcloneInstance) RunLsjson(commandOutputLogFile *os.File, dir strin
 		"-R", dir)
 	rclone.Log.Info("running command", "cmd", cmd.String())
 
-	if _, err := os.Stat(dir); errors.Is(err, os.ErrNotExist) {
-		return merry.Errorf("directory does not exist: %w", err)
-	}
-
 	cmd.Stderr = commandOutputLogFile
 	cmd.Stdout = commandOutputLogFile
 	// Has to be Run so we can redirect output to only the file
 	// Too bulky otherwise
 	err := cmd.Run()
 	if err != nil {
-		return merry.Errorf("failed to run command: %w", err)
+		if strings.Contains(err.Error(), "exit status 3") {
+			return merry.Errorf("directory does not exist: %w", err)
+		}
+		return merry.Errorf("failed to run command (please refer to https://rclone.org/docs/#list-of-exit-codes for more info): %w", err)
 	}
 	rclone.Log.Info("finished running command", "cmd", cmd.String())
 
